8d72b0ad3fe72906c225e7dda7c42ece49258c6c
Tomus69 <dacunha.tom@gmail.com>
2022-10-17 15:23:16 +0200

    Add input file

--- components/SearchAddress.js
+++ components/SearchAddress.js
@@ -1,15 +1,18 @@
 const address = document.querySelector("#inputAddress");
 const list = document.querySelector("#datalistOptions");
 
-address.addEventListener('input', async (e) => {
+address.addEventListener('input', (e) => {
     e.preventDefault();
     if (e.currentTarget.value.length < 4) return;
-    const ad = await fetch(`https://api-adresse.data.gouv.fr/search/?q=${e.currentTarget.value}&limit=5`)
-    const res = await ad.json();
-    res.features.forEach((f) => {
-        const op = document.createElement("option");
-        op.setAttribute("value", f.properties.label);
-        op.innerHTML = f.properties.label;
-        list.appendChild(op);
-    });
+    fetch(`https://api-adresse.data.gouv.fr/search/?q=${e.currentTarget.value}&limit=5`).then((res) => {
+        res.json().then((r) => {
+            r.features.forEach((f) => {
+                console.log(f.properties.label);
+                const op = document.createElement("option");
+                op.setAttribute("value", f.properties.label);
+                op.innerHTML = f.properties.label;
+                list.appendChild(op);
+            });
+        })
+    })
 });
