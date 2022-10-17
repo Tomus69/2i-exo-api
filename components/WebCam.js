3cf0b93d9add91bfb1b4e5e6fd66949f07493c79
Tomus69 <dacunha.tom@gmail.com>
2022-10-17 12:03:00 +0200

    Add webcam

--- components/WebCam.js
+++ components/WebCam.js
@@ -3,13 +3,20 @@ let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
 
-camera_button.addEventListener('click', async function() {
-   	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-	video.srcObject = stream;
-    console.log('camera_button');
-});
+camera_button.addEventListener('click', function(e) {
+	e.preventDefault()
+	navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+	.then((stream) => {
+	  video.srcObject = stream;
+	  video.play();
+	})
+	.catch((err) => {
+	  console.error(`An error occurred: ${err}`);
+	});
+}, false);
 
-click_button.addEventListener('click', function() {
+click_button.addEventListener('click', function(e) {
+	e.preventDefault();
    	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
    	let image_data_url = canvas.toDataURL('image/jpeg');
 
